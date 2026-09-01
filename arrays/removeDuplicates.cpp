5190b0e175b3b0adacc0401aa6a9041fc1812b18
Sana-145 <sanasiddiqua575@gmail.com>
2026-09-01 22:11:02 +0530

    feat(dsa): Remove Duplicates in-place from Sorted Array

--- arrays/removeDuplicates.cpp
+++ arrays/removeDuplicates.cpp
@@ -3,19 +3,37 @@
 #include<vector>
 using namespace std;
 
-vector<int> removeDuplicates(const vector<int>& arr, int n) {
-    if (n == 0 || n == 1) return arr;
+int removeDuplicates(vector<int>& arr, int n) {
+    if (n == 0) return 0;
 
+    int i = 0, j = 1;
+    
+    while (j < n) {
+        if (arr[i] == arr[j]) {
+            j++;
+        } else {
+            i++;
+            arr[i] = arr[j];
+            j++;
+        }
+    }
+
+    return i + 1;
 }
 
 int main() {
     int n;
     cin >> n;
 
-    vector<int> arr;
+    vector<int> arr(n);
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+
+    int result_size = removeDuplicates(arr, n);
+
+    cout << result_size << endl;
+
     return 0;
 }

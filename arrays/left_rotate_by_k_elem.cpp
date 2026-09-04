a7ec7405b8cec3ff5a6afe9dd0a173c66b05f586
Sana-145 <sanasiddiqua575@gmail.com>
2026-09-04 05:59:02 +0530

    fix(dsa): Rotate array by K elements to Left

--- arrays/left_rotate_by_k_elem.cpp
+++ arrays/left_rotate_by_k_elem.cpp
@@ -13,26 +13,19 @@ void reverseArray(vector<int>& arr, int start, int end) {
     }
 }
 
-vector<int> rotateLeft(vector<int>& arr, int n, int k) {
+void rotateLeft(vector<int>& arr,int k) {
+    int n = arr.size();
+
+    if (n == 0 || k == 0) return;
+
     k = k % n; // Handle cases where k is larger than the array size
 
     // Using built-in functions....
-    // // Reverse the first k elements
-    // rotate(arr.begin(), arr.begin() + k);
-    // // Reverse the remaining elements
-    // rotate(arr.begin() + k, arr.end());
-    // // Reverse the entire array
-    // rotate(arr.begin(), arr.end());
-
-    if (n == 0 || k == 0) {
-        return arr;
-    }
+    // rotate(arr.begin(), arr.begin() + k, arr.end());
 
     reverseArray(arr, 0, k - 1);
     reverseArray(arr, k, n - 1);
     reverseArray(arr, 0, n - 1);
-
-    return arr;
 }
 
 int main() {
@@ -45,10 +38,10 @@ int main() {
         cin >> arr[i];
     }
 
-    vector <int> rotatedArray = rotateLeft(arr, n, k);
+    rotateLeft(arr, k);
 
     for (int i = 0; i < n; i++) {
-        cout << rotatedArray[i] << " ";
+        cout << arr[i] << " ";
     }
 
     return 0;
